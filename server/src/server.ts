ef15b8802ebe4f7c8c9c95a4347783da3b265a7d
Moa Hedendahl <moahedendahl@gmail.com>
2023-04-17 16:05:23 +0200

    Installed react, yup, mui

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,19 @@
 // import { app } from './app';
 
 // HÄR SKRIVER NI KODEN FÖR ATT ANSLUTA TILL DATABASEN OCH STARTA SERVERN!
+// import { MongoClient } from "mongodb";
+
+// const url =
+//   "mongodb+srv://admin:mmqkz2H7uDhNSbpz@ubp-assignment.r5vwm1v.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(url);
+// const dbName = "ubp-assignment";
+
+// async function main() {
+//   await client.connect();
+//   console.log("Connected to the database server");
+//   const db = client.db(dbName);
+//   const collection = db.collection("bots");
+//   collection.collectionName;
+// }
+
+// main().catch(console.error);
