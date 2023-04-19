406540f33bb0e59d8d91e5a7da60094182b988d7
Moa Hedendahl <moahedendahl@gmail.com>
2023-04-19 13:29:50 +0200

    Created delete for posts

--- server/src/resources/posts/post-routes.ts
+++ server/src/resources/posts/post-routes.ts
@@ -10,6 +10,16 @@ const postRouter = express
   .post("/api/posts", async (req, res) => {
     const post = await PostModel.create(req.body);
     res.json(post);
+  })
+  .delete("/api/posts/:id", async (req, res) => {
+    const postId = req.params.id;
+    const deletedPost = await PostModel.findOneAndDelete({ _id: postId });
+
+    if (deletedPost) {
+      res.status(204).json(deletedPost);
+    } else {
+      res.status(404).json({ error: `Post with ID ${postId} not found.` });
+    }
   });
 
 export default postRouter;
