5b24dce13436d2e067cbb9581e0a68d24b183ee3
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-04-20 10:29:41 +0200

    added .get for a specific post

--- server/src/resources/posts/post-routes.ts
+++ server/src/resources/posts/post-routes.ts
@@ -8,6 +8,16 @@ const postRouter = express
     const posts = await PostModel.find({});
     res.status(200).json(posts);
   })
+  .get("/api/posts/:id", async (req, res) => {
+    const postId = req.params.id;
+    const post = await PostModel.findById(postId);
+
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ error: `Post with ID ${postId} not found.` });
+    }
+  })
   .post("/api/posts", isAuthenticated, async (req, res) => {
     const userId = req.session!.userId;
     const post = await PostModel.create({ ...req.body, author: userId });
