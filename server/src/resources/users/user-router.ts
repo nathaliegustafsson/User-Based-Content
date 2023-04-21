917368a603202951a16587507112f2e78ff22f94
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-04-21 10:25:13 +0200

    refactored getAllUsers

--- server/src/resources/users/user-router.ts
+++ server/src/resources/users/user-router.ts
@@ -1,13 +1,14 @@
 import express from "express";
-import { loginUser, logoutUser, registerUser } from "./user-controller";
-import UserModel from "./user-model";
+import {
+  getAllUsers,
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "./user-controller";
 
 const userRouter = express
   .Router()
-  .get("/api/users", async (req, res) => {
-    const users = await UserModel.find({});
-    res.json(users);
-  })
+  .get("/api/users", getAllUsers)
   .post("/api/users/register", registerUser)
   .post("/api/users/login", loginUser)
   .post("/api/users/logout", logoutUser);
