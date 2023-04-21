ca79006d1ed06a3d29aa1e26bc5a19d4ef074d12
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-04-21 15:30:41 +0200

    added validation

--- server/src/resources/users/user-controller.ts
+++ server/src/resources/users/user-controller.ts
@@ -1,6 +1,8 @@
 import argon2 from "argon2";
 import { Request, Response } from "express";
-import UserModel from "./user-model";
+import * as Yup from "yup";
+import { UserModel } from "./user-model";
+import { userRegistrationSchema } from "./user-validation";
 
 export function getLoggedInUserInfo(req: Request, res: Response) {
   if (!req.session?.username) {
@@ -17,25 +19,32 @@ export async function getAllUsers(req: Request, res: Response) {
 
 // Register user
 export async function registerUser(req: Request, res: Response) {
-  const { username, password, adminSecret } = req.body;
+  try {
+    // Validate request body
+    const validatedData = await userRegistrationSchema.validate(req.body);
 
-  // Check if the username already exists
-  const existingUser = await UserModel.findOne({ username });
-  if (existingUser) {
-    return res.status(400).json("Username is already in use");
+    // Check if a user with the same username already exists
+    const existingUser = await UserModel.findOne({
+      username: validatedData.username,
+    });
+    if (existingUser) {
+      res
+        .status(400)
+        .json("Username already exists. Please choose a different one.");
+      return;
+    }
+
+    // Create and save the new user
+    const user = new UserModel(validatedData);
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    if (error instanceof Yup.ValidationError) {
+      res.status(400).json(error.message);
+    } else {
+      res.status(500).json("An unexpected error occurred.");
+    }
   }
-
-  const hashedPassword = await argon2.hash(password);
-  // If the adminSecret matches, set the user as an admin
-  const isAdmin = adminSecret === process.env.ADMIN_SECRET;
-  const user = new UserModel({
-    username,
-    password: hashedPassword,
-    isAdmin,
-  });
-
-  await user.save();
-  res.status(201).json(user);
 }
 
 // Login user
@@ -44,21 +53,26 @@ export async function loginUser(req: Request, res: Response) {
 
   const user = await UserModel.findOne({ username });
   if (!user) {
-    return res.status(400).json("Incorrect username or password");
+    return res.status(401).json("Incorrect username");
   }
-
   const isAuth = await argon2.verify(user.password, password);
   if (!isAuth) {
-    return res.status(400).json("Incorrect username or password");
+    return res.status(401).json("Incorrect password");
   }
-
   // Check session/cookie
   req.session!.username = user.username;
   req.session!.userId = user.id; // Stores user ID in the session
-  req.session!.isAdmin = user.isAdmin; // Stores isAdmin status in the session
+  // req.session!.isAdmin = user.isAdmin; // Stores isAdmin status in the session
+
+  // Create a new user object without the password field
+  const userResponse = {
+    _id: user._id,
+    username: user.username,
+    isAdmin: user.isAdmin,
+  };
 
   // Send response
-  res.status(200).json("Login successful");
+  res.status(200).json(userResponse);
 }
 
 // Logout user
@@ -66,7 +80,6 @@ export function logoutUser(req: Request, res: Response) {
   if (!req.session) {
     return res.status(401).json("You are not logged in!");
   }
-
   req.session = null;
-  res.status(200).json("Logout successful");
+  res.status(204);
 }
