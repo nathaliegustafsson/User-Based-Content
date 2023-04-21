e4b6bfc8dab39dc411b62ef4c56621de2c27d2ec
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-04-21 17:22:59 +0200

    using the createPost function we did in school

--- server/src/resources/posts/post-controller.ts
+++ server/src/resources/posts/post-controller.ts
@@ -1,9 +1,7 @@
 import { Request, Response } from "express";
 import "express-async-errors";
 import { Types as MongooseTypes } from "mongoose";
-import * as Yup from "yup";
 import { PostModel } from "./post-model";
-import postValidationSchema from "./post-validation";
 
 export async function getAllPosts(req: Request, res: Response) {
   const posts = await PostModel.find({});
@@ -25,21 +23,6 @@ export async function getPostById(req: Request, res: Response) {
 }
 
 export async function createPost(req: Request, res: Response) {
-  try {
-    await postValidationSchema.validate(req.body); // Validate the request body
-    const userId = req.session!.userId;
-    const post = await PostModel.create({ ...req.body, author: userId });
-    res.json(post);
-  } catch (error) {
-    if (error instanceof Yup.ValidationError) {
-      return res.status(400).json(error.message);
-    } else {
-      return res.status(500).json("An unexpected error occurred.");
-    }
-  }
-}
-
-export async function createPostTest(req: Request, res: Response) {
   // validate body
   // ensure user is logged in
   if (!req.session?.user) {
