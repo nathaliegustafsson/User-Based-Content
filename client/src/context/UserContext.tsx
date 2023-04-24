ddb5e6ddbaf3343c12498492b01968856a1e4734
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-04-24 15:46:38 +0200

    add registeruser and loginuser in context

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -1,28 +1,70 @@
 import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 interface User {
   username: string;
   password: string;
 }
 
+interface Props {
+  children: React.ReactNode;
+}
+
 interface UserContextProps {
   user: User | null;
   login: (username: string, password: string) => void;
   logout: () => void;
+  register: (username: string, password: string) => void;
 }
 
 const UserContext = createContext<UserContextProps>({
   user: null,
   login: () => {},
   logout: () => {},
+  register: () => {},
 });
 
-const UserProvider: React.FC<UserContextProps> = () => {
+export const useUserContext = () => useContext(UserContext);
+
+export const UserProvider = ({ children }: Props) => {
   const [user, setUser] = useState<User | null>(null);
-  const handleLogin = (username: string, password: string) => {
-    // Here you can implement the logic to authenticate the user
-    // and set the user state accordingly
-    setUser({ username, password });
+
+  const RegisterUser = async (username: string, password: string) => {
+    try {
+      const response = await fetch("/api/users/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to register user");
+      }
+      const user = await response.json();
+      setUser(user);
+      LogInUser(user.username, user.password);
+      return user;
+    } catch (error: any) {
+      throw new Error(error.message || "Failed to register user");
+    }
+  };
+
+  const LogInUser = async (username: string, password: string) => {
+    try {
+      const response = await fetch("/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to log in user");
+      }
+      const user = await response.json();
+      setUser(user);
+      return user;
+    } catch (error: any) {
+      throw new Error(error.message || "Failed to log in user");
+    }
   };
 
   const handleLogout = () => {
@@ -31,12 +73,14 @@ const UserProvider: React.FC<UserContextProps> = () => {
 
   return (
     <UserContext.Provider
-      value={{ user, login: handleLogin, logout: handleLogout }}
+      value={{
+        user,
+        register: RegisterUser,
+        login: LogInUser,
+        logout: handleLogout,
+      }}
     >
+      {children}
     </UserContext.Provider>
   );
 };
-
-const useUserContext = () => useContext(UserContext);
-
-export { UserProvider, useUserContext };
