61e3a550b11da2635c63d2876eb333fb3cf59969
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-04-25 13:57:23 +0200

    edited postCreateValidationSchema with Yup's own error messages

--- server/src/resources/posts/post-create-validation.ts
+++ server/src/resources/posts/post-create-validation.ts
@@ -2,8 +2,8 @@ import * as Yup from "yup";
 
 const postCreateValidationSchema = Yup.object()
   .shape({
-    title: Yup.string().required("Title is required"),
-    content: Yup.string().required("Content is required"),
+    title: Yup.string().required().strict(),
+    content: Yup.string().required().strict(),
   })
   .noUnknown("Additional properties are not allowed");
 
