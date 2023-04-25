bca15bf8b226a41c1e486d31d0b52e590f064469
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-04-25 13:58:07 +0200

    edited postUpdateSchema with Yup's own validation messages

--- server/src/resources/posts/post-update-validation.ts
+++ server/src/resources/posts/post-update-validation.ts
@@ -2,14 +2,11 @@ import * as Yup from "yup";
 
 const postUpdateValidationSchema = Yup.object()
   .shape({
-    title: Yup.string()
-      .notRequired()
-      .when("content", (content, schema) =>
-        content
-          ? schema
-          : schema.required("At least one field to update is required")
-      ),
-    content: Yup.string().notRequired(),
+    _id: Yup.string().required(),
+    title: Yup.string().required(),
+    content: Yup.string().required(),
+    author: Yup.string().required(),
+    createdAt: Yup.string().required(),
   })
   .noUnknown("Additional properties are not allowed");
 
