dea10ee3123f99f6fe9de0b0cd4742381749f235
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-04-25 13:59:24 +0200

    removed isAdmin (user can't be admin when registering)

--- server/src/resources/users/user-validation.ts
+++ server/src/resources/users/user-validation.ts
@@ -1,7 +1,6 @@
 import * as Yup from "yup";
 
 export const userRegistrationSchema = Yup.object().shape({
-  username: Yup.string().required("Username is required"),
-  password: Yup.string().required("Password is required"),
-  isAdmin: Yup.boolean().notRequired(),
+  username: Yup.string().required().strict(),
+  password: Yup.string().required().strict(),
 });
