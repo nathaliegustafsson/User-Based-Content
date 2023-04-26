fb9fc1b0e50f27a9be34537cb119ed65b392553a
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-04-26 19:33:55 +0200

    added PostCard component

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -1,75 +1,108 @@
-// PostPage.tsx
 import {
   Box,
   CardContent,
   CardMedia,
   Container,
-  Grid,
   Paper,
+  SxProps,
+  Theme,
   Typography,
+  styled,
 } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Post, usePostContext } from "../context/PostContext";
 
 function PostCard() {
-  const { id } = useParams<{ id: string }>();
+  const { _id } = useParams<{ _id: string }>();
   const { getPostById } = usePostContext();
   const [post, setPost] = useState<Post | null>(null);
 
   useEffect(() => {
-    if (id) {
+    console.log("useEffect triggered");
+    if (_id) {
       const fetchSinglePost = async () => {
-        const fetchedPost = await getPostById(id);
-        setPost(fetchedPost);
+        const fetchedPost = await getPostById(_id);
+        console.log("fetchedPost", fetchedPost);
+        if (fetchedPost) {
+          setPost(fetchedPost);
+        }
       };
       fetchSinglePost();
     }
-  }, [id, getPostById]);
+  }, [_id, getPostById]);
 
+  console.log("post", post);
   if (!post) {
     return <div>Sorry! The post was not found.</div>;
   }
 
   return (
-    <Container maxWidth="md">
-      <Paper elevation={3}>
-        <Grid container>
-          <Grid key={post.id} item xs={12} md={6}>
-            <CardMedia
-              component="img"
-              image={post?.content}
-              alt={post?.title}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <CardContent>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "center",
-                  marginBottom: "1rem",
-                  gap: "1rem",
-                }}
-              >
-                {/* <Avatar src={post.avatar} alt={post?.title} /> */}
-                <Box>
-                  <Typography variant="subtitle1">{post?.username}</Typography>
-                  <Typography variant="caption" sx={{ color: "#777777" }}>
-                    {/* {post?.location} */}
-                  </Typography>
-                </Box>
-              </Box>
-              <Typography variant="h6" sx={{ marginTop: "1rem" }}>
-                {post?.title}
-              </Typography>
-            </CardContent>
+    <Container maxWidth="lg" sx={rootStyle}>
+      <Box>
+        <Grid container rowSpacing={2} columnSpacing={2}>
+          <Grid key={`${post?._id}-${post?.timestamp}`} xs={12} sm={12} md={12}>
+            <Item
+              sx={{
+                display: "flex",
+                flexDirection: { xs: "column", md: "row" },
+              }}
+            >
+              <CardMedia>
+                <img src={post?.content} alt={post?.title} />
+              </CardMedia>
+              <CardContent sx={cardContentStyle}>
+                <Typography variant="overline" sx={{ marginBottom: "1rem" }}>
+                  {post?.title}
+                </Typography>
+                <Typography variant="h5" sx={{ marginBottom: "1rem" }}>
+                  {post?.title}
+                </Typography>
+              </CardContent>
+            </Item>
           </Grid>
         </Grid>
-      </Paper>
+      </Box>
     </Container>
   );
 }
+
+/*  Styling */
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.subtitle2,
+  textAlign: "left",
+  color: theme.palette.text.primary,
+  cursor: "pointer",
+  transition: "box-shadow 0.3s ease-in-out",
+  boxShadow: "0 .125rem .625rem rgba(0, 0, 0, 0.2)",
+  "&:hover": {
+    backgroundColor: "#FFFFFF",
+  "& img": {
+    width: "100%",
+    height: "auto",
+    objectFit: "cover", // Ensures that the image fills the available space while preserving its aspect ratio
+  },
+}));
+
+const cardContentStyle: SxProps<Theme> = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+  textAlign: "left",
+  "@media (min-width: 960px)": {
+    // Media query for desktop view
+    width: "100%",
+    height: "auto",
+    paddingLeft: "2rem",
+  },
+};
+
+const rootStyle: SxProps<Theme> = {
+  display: "flex",
+  justifyContent: "center",
+  marginTop: "1.5rem",
+};
 
 export default PostCard;
