0a32699756b8c61be912695d841d7955a5fe9b71
Moa Hedendahl <moahedendahl@gmail.com>
2023-04-26 15:33:02 +0200

    Maybe made edit post to work, need to be able to get post to test (wip)

--- client/src/components/EditPost.tsx
+++ client/src/components/EditPost.tsx
@@ -2,7 +2,6 @@ import {
   Avatar,
   Box,
   Button,
-  CircularProgress,
   Container,
   IconButton,
   TextField,
@@ -10,22 +9,26 @@ import {
   useMediaQuery,
   useTheme,
 } from "@mui/material";
-import * as React from "react";
-import { useEffect, useState } from "react";
+import { useFormik } from "formik";
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { usePostContext } from "../context/PostContext";
+import * as Yup from "yup";
+import { Post, usePostContext } from "../context/PostContext";
+
+const EditSchema = Yup.object({
+  title: Yup.string().required("Write something"),
+  content: Yup.string().required("Write url"),
+});
+
+export type EditValues = Yup.InferType<typeof EditSchema>;
 
 function EditPost({ postId }: { postId: number }) {
-  const navigate = useNavigate();
-  const [post, setPost] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [formData, setFormData] = useState({
-    title: "",
-    content: "",
-  });
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
-  const { getPost } = usePostContext();
+  const navigate = useNavigate();
+  const { getPost, updatePost } = usePostContext();
+  const [post, setPost] = React.useState<Post | null>(null);
+  const isEdit = Boolean(post);
 
   React.useEffect(() => {
     async function fetchPost() {
@@ -36,44 +39,18 @@ function EditPost({ postId }: { postId: number }) {
     fetchPost();
   }, [postId]);
 
-  useEffect(() => {
-    async function fetchPost() {
-      const response = await fetch(`/api/posts/${postId}`);
-      const post = await response.json();
-      setPost(post);
-      setFormData({
-        title: post.title,
-        content: post.content,
-      });
-      setLoading(false);
-    }
-    fetchPost();
-  }, [postId]);
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [event.target.name]: event.target.value,
-    }));
-  };
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setLoading(true);
-    await fetch(`/api/posts/${postId}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-    setLoading(false);
-    navigate(`/posts/${postId}`);
-  };
-
-  if (loading) {
-    return <CircularProgress />;
-  }
+  const formik = useFormik<EditValues>({
+    initialValues: {
+      title: isEdit ? post?.title ?? "" : "",
+      content: isEdit ? post?.content ?? "" : "",
+    },
+    validationSchema: EditSchema,
+    onSubmit: (post) => {
+      if (isEdit) {
+        updatePost(post);
+      }
+    },
+  });
 
   return (
     <Container maxWidth={"md"}>
@@ -97,7 +74,8 @@ function EditPost({ postId }: { postId: number }) {
         <Container sx={{ display: "flex", flexDirection: "column" }}>
           <Box
             component="img"
-            // src={post?.content}
+            // src="https://user-images.githubusercontent.com/116926631/233002457-be833494-2c6d-4c8a-9932-81a0791893b6.JPG"
+            src={post?.content}
             sx={{
               width: "100%",
               marginTop: isSmallScreen ? "1rem" : "0",
@@ -137,29 +115,33 @@ function EditPost({ postId }: { postId: number }) {
               </Typography>
             </Box>
             <Box>
-              <form>
+              <form onSubmit={formik.handleSubmit}>
                 <TextField
                   fullWidth
+                  id="title"
+                  type="text"
                   label="Title"
                   name="title"
-                  value={formData.title}
-                  onChange={handleChange}
+                  value={formik.values.title}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  error={Boolean(formik.touched.title && formik.errors.title)}
+                  helperText={formik.touched.title && formik.errors.title}
                   sx={{ marginTop: "1rem" }}
                 />
+                <Container
+                  sx={{
+                    padding: "0px !important",
+                  }}>
+                  <Button
+                    variant="contained"
+                    type="submit"
+                    sx={{ marginRight: "0.5rem", marginTop: "1rem" }}>
+                    Save
+                  </Button>
+                </Container>
               </form>
             </Box>
-          </Container>
-          <Container
-            sx={{
-              padding: "0px !important",
-            }}>
-            <Button
-              component={Link}
-              to="/user/:id/edit/post"
-              variant="contained"
-              sx={{ marginRight: "0.5rem" }}>
-              Save
-            </Button>
           </Container>
         </Container>
       </Container>
