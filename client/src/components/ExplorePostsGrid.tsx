8b9a03283975c3d7c6c9e40b6845f75ea2e70226
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-04-27 15:31:32 +0200

    added authorPostGrid that disappeard on merge

--- client/src/components/ExplorePostsGrid.tsx
+++ client/src/components/ExplorePostsGrid.tsx
@@ -79,7 +79,7 @@ function ExplorePostsGrid() {
                         />
                         <Box>
                           <Typography variant="subtitle1">
-                            {post.author.username}
+                            {post.authorPostGrid}
                           </Typography>
                           <Typography
                             variant="caption"
