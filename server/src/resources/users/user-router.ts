1c51283e331e2fc601dc89f3c137382b4489907f
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-06-16 13:01:56 +0200

    removed comment

--- server/src/resources/users/user-router.ts
+++ server/src/resources/users/user-router.ts
@@ -17,6 +17,5 @@ const userRouter = express
   .post("/api/users/login", loginUser)
   .post("/api/users/logout", logoutUser)
   .get("/api/users/auth", isAuthenticated, getLoggedInUser)
-  // .get("/api/users/:username", getSpecificUser)
   .post("/api/users/checkUsername", checkUsername);
 export default userRouter;
